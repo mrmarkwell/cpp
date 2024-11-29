22d1ad1ee652bbe37ad07b467360ae593a5f0a27
Matthew Markwell <markwell@google.com>
2024-11-29 14:09:28 -0800

    Just some globals in objdump example.

--- objdump/main.cc
+++ objdump/main.cc
@@ -1,5 +1,8 @@
 #include "square.h"
 
+int example_global_int = 0xDEADBEEF;
+int example_uninitialized_long;
+
 int main(int argc, char** argv) {
   // Return the square of the number of arguments.
   return Square(argc - 1);
